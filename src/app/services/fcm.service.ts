c10d1e4a46eba44d6e94c9df9017e4e5f9b815c9
Daniel C Rubiano <dcrubiano01@gmail.com>
2021-09-28 19:12:21 -0500

    First commit

---
+++ src/app/services/fcm.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed,
+  Capacitor
+} from '@capacitor/core';
+import { Router } from '@angular/router';
+
+const { PushNotifications } = Plugins;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FcmService {
+
+  constructor(private router: Router) { }
+
+  initPush() {
+    if (Capacitor.platform !== 'web') {
+      this.registerPush();
+    }
+  }
+
+  private registerPush() {
+    PushNotifications.requestPermission().then((permission) => {
+      if (permission.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // No permission for push granted
+      }
+    });
+
+    PushNotifications.addListener(
+      'registration',
+      (token: PushNotificationToken) => {
+        alert('Push registration success, token: ' + token.value);
+        console.log('My Token: ');
+        console.log(JSON.stringify(token));
+
+      }
+    );
+
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.log('Error: ' + JSON.stringify(error));
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      async (notification: PushNotification) => {
+        console.log('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      async (notification: PushNotificationActionPerformed) => {
+        const data = notification.notification.data;
+        console.log('Action performed: ' + JSON.stringify(notification.notification));
+        if (data.detailsId) {
+          this.router.navigateByUrl(`/home/${data.detailsId}`);
+        }
+      }
+    );
+  }
+}
